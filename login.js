b2faaa573977a457c14bae791783834a15dc0d24
Kashisk2 <kashiramshekhda2010@gmail.com>
2022-08-05 00:13:19 +0530

    done

--- login.js
+++ login.js
@@ -8,14 +8,20 @@ function logIn() {
     var password = document.getElementById("Password").value;
 
     var ans = false;
+    if(email==""||password==""){
+        alert("Please enter correct details");
+    }else{
     for(var i = 0; i < user.length; i++) {
         if(user[i].Email == email && user[i].Password == password) {
             ans = true;
         }
     }
+
     if(ans == true) {
         alert("login sccessfull !");
+        location="homePage.html"
     } else {
         alert("Wrong Credentials");
     }
+}
 }
--- wishlist.js
+++ wishlist.js
@@ -1,10 +1,61 @@
 // console.log("Yes");
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function (event) {
+    if (!event.target.matches(".dropbtn")) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains("show")) {
+          openDropdown.classList.remove("show");
+        }
+      }
+    }
+  };
 var wishlistArr=JSON.parse(localStorage.getItem("wish")) || [];
-document.getElementById("wishlist").addEventListener("click",displaywish);
+window.addEventListener("load", displaywish);
+document.getElementById("wishlist").addEventListener("click",displaywish)
 
 function displaywish(){
     
     document.getElementById("data").innerText="";
+     if (wishlistArr.length == 0) {
+       var div = document.createElement("div");
+       div.setAttribute("id", "innerdiv");
+       var img = document.createElement("img");
+       img.setAttribute(
+         "src",
+         "https://cdn.pixabay.com/photo/2017/02/18/02/59/application-2076445__340.png"
+       );
+       img.setAttribute("id", "img");
+       img.style.margin="20px auto"
+       var h4 = document.createElement("h4");
+       h4.innerText = "No jobs saved yet";
+       h4.setAttribute("id","h4");
+
+       var button = document.createElement("button");
+       button.innerText = "Find Job 👉";
+       button.addEventListener("click", function () {
+         location = "homePage.html";
+       });
+
+       // div.append(img,button);
+
+       button.setAttribute("id", "butt");
+       button.style.width="100px"
+       button.style.margin="10px auto 10px auto"
+
+    //    document.getElementById("data").style.display = "flex";
+    //    document.getElementById("data").style.alignItems = "center";
+    //    document.getElementById("data").style.flexDirection = "column";
+
+       document.getElementById("data").append(img, h4, button);
+     }
+     else{
     wishlistArr.map(function(ele,index){
         var div=document.createElement("div");
         var div1=document.createElement("div");
@@ -68,6 +119,7 @@ function displaywish(){
 
     })
 }
+}
 
 function applyNow(index){
     var f=0;
@@ -128,6 +180,39 @@ document.getElementById("applied").addEventListener("click",displayapplied);
 function displayapplied(){
 
     document.getElementById("data").innerText="";
+    if (appliedArr.length == 0) {
+       var div = document.createElement("div");
+       div.setAttribute("id", "innerdiv");
+       var img = document.createElement("img");
+       img.setAttribute(
+         "src",
+         "https://cdn.pixabay.com/photo/2018/07/15/15/12/observe-3539810__340.jpg"
+       );
+       img.setAttribute("id", "img");
+       img.style.margin="20px auto"
+       var h4 = document.createElement("h4");
+       h4.innerText = "No jobs apply  yet";
+       h4.setAttribute("id","h4");
+
+       var button = document.createElement("button");
+       button.innerText = "Find Job 👉";
+       button.addEventListener("click", function () {
+         location = "homePage.html";
+       });
+
+       // div.append(img,button);
+
+       button.setAttribute("id", "butt");
+       button.style.width="100px"
+       button.style.margin="10px auto 10px auto"
+
+    //    document.getElementById("data").style.display = "flex";
+    //    document.getElementById("data").style.alignItems = "center";
+    //    document.getElementById("data").style.flexDirection = "column";
+
+       document.getElementById("data").append(img, h4, button);
+     }
+     else{
     appliedArr.map(function(ele,index){
         var div=document.createElement("div");
         var div1=document.createElement("div");
@@ -185,6 +270,7 @@ function displayapplied(){
         });
 
     });
+}
 }
 
 function changeStatusInApplied(index){
