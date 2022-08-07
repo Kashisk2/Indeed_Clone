225e1439c68566922cbaa0d5140de373c5ebf70c
Kashisk2 <kashiramshekhda2010@gmail.com>
2022-08-07 21:18:35 +0530

    done

--- signup.js
+++ signup.js
@@ -7,9 +7,18 @@ function signUp() {
     var email = document.getElementById("email").value;
     var Password = document.getElementById("Password").value;
     console.log(name);
+    var flg=0;
+    formData.map(function(elm){
+        if(elm.Email==email&&elm.Email!=""){
+          
+            flg=1;
+        }
+    })
+    if(flg==0){
     if(name==""||email==""||Password==""){
         alert("Please enter details");
-    }else{
+    }
+    else{
     var data = {
         Name: name,
         Email: email,
@@ -19,6 +28,9 @@ function signUp() {
     formData.push(data);
     localStorage.setItem("user", JSON.stringify(formData));
     alert("Sign Up successfull!");
+}
+}else{
+      alert("Account alredy exist\n Try to login");
 }
 }
 
